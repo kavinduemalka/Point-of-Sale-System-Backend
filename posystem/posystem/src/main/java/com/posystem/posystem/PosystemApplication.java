a914a48df3d6339e7683bc2851ff7506a9a6df39
kavinduemalka <kavinduemalka1@gmail.com>
2024-05-23 22:46:28 +0530

    Initial Commit

---
+++ posystem/posystem/src/main/java/com/posystem/posystem/PosystemApplication.java
@@ -0,0 +1,13 @@
+package com.posystem.posystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosystemApplication.class, args);
+	}
+
+}
